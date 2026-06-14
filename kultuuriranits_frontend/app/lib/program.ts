0795fbe4cde57bc1cfd513bbb659befeb83da737
lisettereins <lreins@tlu.ee>
2026-06-14 12:45:01 +0300

    feat: add getPrograms and getCategories functions to lib

--- kultuuriranits_frontend/app/lib/program.ts
+++ kultuuriranits_frontend/app/lib/program.ts
@@ -13,6 +13,19 @@ export default async function getProgram(programId: string) {
     }
 }
 
+export async function getPrograms() {
+ 
+    const res = await fetch(`${API_URL}/program`, {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      return null;
+    } else {
+        return await res.json();
+    }
+}
+
 export async function getPopularPrograms(): Promise<any> {
     try {
         const res = await fetch(`${API_URL}/program`, {
